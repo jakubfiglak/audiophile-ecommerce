bc99c8591a2ad05bdde3aef31af82ed36cec83f5
Jakub Figlak <jakub.figlak@netguru.pl>
2021-11-13 21:55:42 +0100

    feat: switch to ts in gatsby-config

---
+++ gatsby-config.ts
@@ -0,0 +1,44 @@
+import { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Audiophile e-commerce',
+  },
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-sanity",
+    //   options: {
+    //     projectId: "",
+    //     dataset: "",
+    //   },
+    // },
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+    },
+  ],
+};
+
+export default config;
