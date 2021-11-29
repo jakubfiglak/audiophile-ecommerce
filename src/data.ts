9fda0daa248cc2ef170365036e6003605732afea
Jakub Figlak <jakub.figlak@netguru.pl>
2021-11-29 15:45:03 +0100

    feat: create category card

---
+++ src/data.ts
@@ -0,0 +1,21 @@
+import headphonesCardImage from './assets/images/image-category-thumbnail-headphones.png';
+import speakersCardImage from './assets/images/image-category-thumbnail-speakers.png';
+import earphonesCardImage from './assets/images/image-category-thumbnail-earphones.png';
+
+export const categoriesCardData = [
+  {
+    title: 'headphones',
+    image: headphonesCardImage,
+    linkTo: '/headphones',
+  },
+  {
+    title: 'speakers',
+    image: speakersCardImage,
+    linkTo: '/speakers',
+  },
+  {
+    title: 'earphones',
+    image: earphonesCardImage,
+    linkTo: '/earphones',
+  },
+];
