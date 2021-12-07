d4724c49a91c3a2e9ab2ef36221a1a0fe48b9a36
Jakub Figlak <jakub.figlak@netguru.pl>
2021-12-07 10:46:20 +0100

    fix: convert button props to transient props to avoid passing to underlying react node

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,8 +3,8 @@ import { Link } from 'gatsby';
 import styled, { css } from 'styled-components';
 
 export type StylesProps = {
-  secondary?: boolean;
-  tertiary?: boolean;
+  $secondary?: boolean;
+  $tertiary?: boolean;
 };
 
 const secondaryBorder = css`
@@ -22,39 +22,39 @@ const secondaryBorder = css`
   }
 `;
 
-const styles = ({ secondary, tertiary }: StylesProps) => css`
+const styles = ({ $secondary, $tertiary }: StylesProps) => css`
   display: inline-block;
   font-size: 1.3rem;
   line-height: 1.8rem;
   letter-spacing: 1px;
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   background: ${({ theme }) => theme.colors.primary};
-  background: ${() => secondary && 'transparent'};
-  background: ${({ theme }) => tertiary && theme.colors.black};
+  background: ${() => $secondary && 'transparent'};
+  background: ${({ theme }) => $tertiary && theme.colors.black};
   color: ${({ theme }) =>
-    secondary ? theme.colors.black : theme.colors.white};
+    $secondary ? theme.colors.black : theme.colors.white};
   padding: 1.5rem 3rem;
   border: none;
   text-transform: uppercase;
   cursor: pointer;
   transition: ${({ theme }) => theme.transition};
 
-  ${secondary ? secondaryBorder : ''}
+  ${$secondary ? secondaryBorder : ''}
 
   :hover {
     background: ${({ theme }) => theme.colors.primaryLight};
-    background: ${({ theme }) => secondary && theme.colors.black};
-    background: ${({ theme }) => tertiary && theme.colors.gray};
-    color: ${({ theme }) => (secondary ? theme.colors.white : '')};
+    background: ${({ theme }) => $secondary && theme.colors.black};
+    background: ${({ theme }) => $tertiary && theme.colors.gray};
+    color: ${({ theme }) => ($secondary ? theme.colors.white : '')};
   }
 `;
 
 const StyledButton = styled.button<StylesProps>`
-  ${({ secondary, tertiary }) => styles({ secondary, tertiary })}
+  ${({ $secondary, $tertiary }) => styles({ $secondary, $tertiary })}
 `;
 
 const StyledLink = styled(Link)<StylesProps>`
-  ${({ secondary, tertiary }) => styles({ secondary, tertiary })}
+  ${({ $secondary, $tertiary }) => styles({ $secondary, $tertiary })}
 `;
 
 export type CommonProps = StylesProps & { label: string };
@@ -63,20 +63,24 @@ export type LinkProps = CommonProps & { to: string };
 
 export const Button = ({
   label,
-  secondary,
-  tertiary,
+  $secondary,
+  $tertiary,
   onClick,
 }: ButtonProps) => {
   return (
-    <StyledButton secondary={secondary} tertiary={tertiary} onClick={onClick}>
+    <StyledButton
+      $secondary={$secondary}
+      $tertiary={$tertiary}
+      onClick={onClick}
+    >
       {label}
     </StyledButton>
   );
 };
 
-export const LinkButton = ({ label, secondary, tertiary, to }: LinkProps) => {
+export const LinkButton = ({ label, $secondary, $tertiary, to }: LinkProps) => {
   return (
-    <StyledLink secondary={secondary} tertiary={tertiary} to={to}>
+    <StyledLink $secondary={$secondary} $tertiary={$tertiary} to={to}>
       {label}
     </StyledLink>
   );
