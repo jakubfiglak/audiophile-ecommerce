2c58507af1b87452a93dbcf8c1b84ebaaaea3193
Jakub Figlak <jakub.figlak@netguru.pl>
2021-12-08 10:39:52 +0100

    feat: create go back button

--- src/pages/product/{SanityProduct.slug__current}.tsx
+++ src/pages/product/{SanityProduct.slug__current}.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, navigate } from 'gatsby';
 import type { PageProps } from 'gatsby';
+import styled from 'styled-components';
 import { LayoutContentWrapper } from '../../components/layout/LayoutContentWrapper';
 import { ExtraMenu } from '../../components/menu/ExtraMenu';
 import { AboutSection } from '../../components/about-section/AboutSection';
 import { ProductPageQuery } from '../../../graphql/generated-types';
+
+const StyledGoBackButton = styled.button`
+  margin-top: ${({ theme }) => `calc(${theme.navHeight} + 2rem)`};
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+  opacity: 0.5;
+  background: none;
+  border: none;
+  padding: 0;
+  transition: ${({ theme }) => theme.transition};
+  cursor: pointer;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+type Props = PageProps<ProductPageQuery>;
+
+const ProductPage = ({ data }: Props) => {
+  return (
+    <LayoutContentWrapper>
+      <StyledGoBackButton
+        onClick={() => navigate(-1)}
+        aria-label="Go to previous page"
+      >
+        Go Back
+      </StyledGoBackButton>
+      <ExtraMenu />
+      <AboutSection />
+    </LayoutContentWrapper>
+  );
+};
 
 export const query = graphql`
   query ProductPage($id: String!) {
@@ -36,17 +70,5 @@ export const query = graphql`
     }
   }
 `;
-
-type Props = PageProps<ProductPageQuery>;
-
-const ProductPage = ({ data }: Props) => {
-  return (
-    <LayoutContentWrapper>
-      <pre style={{ marginTop: 200 }}>{JSON.stringify(data, null, 2)}</pre>
-      <ExtraMenu />
-      <AboutSection />
-    </LayoutContentWrapper>
-  );
-};
 
 export default ProductPage;
