cabbc8966e315a54c3de2dbebd8bc528fd6609d0
Jakub Figlak <jakub.figlak@netguru.pl>
2021-12-09 16:09:24 +0100

    feat: create useClickOutside hook\

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { RefObject, useEffect } from 'react';
+
+export function useClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void
+) {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('click', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
